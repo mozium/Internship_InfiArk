8e95b313a08e5b4706282f7d802ebcfb2fd2d8c2
mozium <ChengHsi.Yang@gmail.com>
2020-10-23 23:44:43 +0800

    establishing the basis of backend structure

--- main.py
+++ main.py
@@ -1,35 +1,120 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, render_template, request, session #, url_for
 import pandas as pd
+# import json
+# import random
+# import datetime
+# from datetime import timedelta
 
+# Flask Initialization
 app = Flask(__name__)
-db = SQLAlchemy()
 
-# mysql connection setting
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:666666@IP:3306/web"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://user_name:password@IP:3306/db_name"
+# MySQL Initialization
+import pymysql
+from sqlalchemy import create_engine
 
-db.init_app(app)
+db_reader = pymysql.connect(
+    host = "127.0.0.1",
+    port = int(3306),
+    user = 'root',
+    passwd = '',
+    db = 'web',
+    charset = 'utf8mb4',
+    cursorclass = pymysql.cursors.DictCursor)
 
-@app.route('/')
-class user(db.Model):
-    __tablename__ = 'user'
-    u_id = db.Column(db.Integer, primary_key=True)
-    u_acc = db.Column(db.String(30), unique=True, nullable=False)
-    u_pwd = db.Column(db.Integer, nullable=False)
-    u_type = db.Column(db.Integer, nullable=False)
+db_writer = create_engine('mysql+pymysql://root:666666@127.0.0.1/web')
+usr_data = pd.read_sql_query("SELECT * FROM usr", db_reader)
+db_cursor = db_reader.cursor()
 
-    def __init__(self, u_id, u_acc, u_pwd, u_type):
-        self.u_id = u_id
-        self.u_acc = u_acc
-        self.u_pwd = u_pwd
-        self.u_type = u_type
+# Session Initialization
+app.config['SECRET_KEY'] = 'InfiArk_Internship_2020'
 
 
-@app.route('/')
-def index():
-    # Create data
-    db.create_all()
+@app.route("/")
+def init():
+    if (session.get('usr')):
+        return render_template("main.html")
+    else:
+        return render_template("login.html")
 
-    return 'ok'
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    #login functions
+    if request.method == "POST":
+        lusr = request.values['usr']
+        lpwd = request.values['pwd']
+
+        if lpwd == usr_data[usr_data['acc']==lusr]['pwd'].iloc[0]:
+            session['usr'] = str(lusr)
+            return redirect("/home")
+        else:
+            return redirect("/")
+
+def register():
+    #register functions
+    if request.method == "POST":
+        usr = request.values['rusr']
+        pwd = request.values['rpwd']
+        cnf = request.values['rpwd2']
+        nckn = request.values['rnckn']
+
+        if str(pwd) == str(cnf):
+
+
+            session['usr'] = usr
+            return redirect("/")
+            '''
+            user_list =  open("./schema/user.json", "r",encoding="utf-8")
+            user_data = json.load(user_list)
+
+            n = len(user_data)
+            nstr = str(n)
+            idstr = "p"+nstr
+    
+            nentry = {
+                "username":user,
+                "password":pwd,
+                "nickname":nckn
+            }
+
+            user_data.append(nentry)
+
+            with open(f'./schema/user.json', 'w') as json_file:
+                json.dump(user_data, json_file)
+                json_file.close()
+            '''
+    else:
+        return redirect("/")
+
+
+@app.route("/main.html", methods=['GET', 'POST'])
+def pagerefresh():
+    urls = [
+        'http://www.w3schools.com',
+        'http://techcrunch.com/',
+        'https://www.youtube.com/',
+    ]
+    iframe = random.choice(urls)
+    print(iframe)
+
+    return render_template('main.html', iframe=iframe)
+
+
+@app.route("/login")
+def lock():
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session['usr'] = False
+    return redirect("/")
+
+
+@app.errorhandler(404) # Redirecting undefined URLs
+def page_not_found(e):
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run()
